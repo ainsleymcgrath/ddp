553345871247cc9556291a9f057a5a0a625372ac
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2019-07-22 22:34:24 -0500

    destroy iife, it didn't do anything

--- main.js
+++ main.js
@@ -4,18 +4,16 @@ import {
   onDragStart,
 } from "./modules/eventHandlers.js";
 
-(() => {
-  const circle = document.getElementById("circle");
-  const root = document.getElementById("root");
+const circle = document.getElementById("circle");
+const root = document.getElementById("root");
 
-  circle.style.backgroundColor = "lightsteelblue";
-  root.style.backgroundColor = "snow";
+circle.style.backgroundColor = "lightsteelblue";
+root.style.backgroundColor = "snow";
 
-  [
-    ["mousedown", onMouseDown],
-    ["mouseup", onMouseUp],
-    ["dragstart", onDragStart],
-  ].forEach(([listener, callback]) =>
-    circle.addEventListener(listener, callback, false),
-  );
-})();
+[
+  ["mousedown", onMouseDown],
+  ["mouseup", onMouseUp],
+  ["dragstart", onDragStart],
+].forEach(([listener, callback]) =>
+  circle.addEventListener(listener, callback, false),
+);
